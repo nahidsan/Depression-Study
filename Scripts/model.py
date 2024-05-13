1876b50a8906ca54c3ab08ed8453e4eadf7fd910
Sk Nahid Sanwar <sk.nahid.sanwar@gmail.com>
2024-05-13 23:16:52 +0900

    Update model.py

--- Scripts/model.py
+++ Scripts/model.py
@@ -19,49 +19,7 @@ torch.cuda.manual_seed(args.seed)
 
 
 #Adding Attention Layer Nahid
-'''
-class SelfAttentionLayer(nn.Module):
-    def __init__(self, hidden_size, num_attention_heads, attention_dropout):
-        super(SelfAttentionLayer, self).__init__()
-        self.num_attention_heads = num_attention_heads
-        self.attention_head_size = int(hidden_size / num_attention_heads)
-        self.all_head_size = self.num_attention_heads * self.attention_head_size
 
-        self.query = nn.Linear(hidden_size, self.all_head_size)
-        self.key = nn.Linear(hidden_size, self.all_head_size)
-        self.value = nn.Linear(hidden_size, self.all_head_size)
-
-        self.dropout = nn.Dropout(attention_dropout)
-
-    def transpose_for_scores(self, x):
-        new_x_shape = x.size()[:-1] + (self.num_attention_heads, self.attention_head_size)
-        x = x.view(*new_x_shape)
-        return x.permute(0, 2, 1, 3)
-
-    def forward(self, hidden_states, attention_mask):
-        mixed_query_layer = self.query(hidden_states)
-        mixed_key_layer = self.key(hidden_states)
-        mixed_value_layer = self.value(hidden_states)
-
-        query_layer = self.transpose_for_scores(mixed_query_layer)
-        key_layer = self.transpose_for_scores(mixed_key_layer)
-        value_layer = self.transpose_for_scores(mixed_value_layer)
-
-        attention_scores = torch.matmul(query_layer, key_layer.transpose(-1, -2))
-        attention_scores = attention_scores / math.sqrt(self.attention_head_size)
-        attention_scores = attention_scores + attention_mask
-
-        attention_probs = nn.Softmax(dim=-1)(attention_scores)
-        attention_probs = self.dropout(attention_probs)
-
-        context_layer = torch.matmul(attention_probs, value_layer)
-        context_layer = context_layer.permute(0, 2, 1, 3).contiguous()
-        new_context_layer_shape = context_layer.size()[:-2] + (self.all_head_size,)
-        context_layer = context_layer.view(*new_context_layer_shape)
-
-        return context_layer
-
-'''
 
 #Adding to debug
 
@@ -78,24 +36,17 @@ class SelfAttentionLayer(nn.Module):
 
         self.dropout = nn.Dropout(attention_dropout)
 
-    '''
+   
+
+#Added to resolve the dimension error
     def transpose_for_scores(self, x):
         print("Input shape:", x.shape)
+        # Add an additional dimension to make it 4-dimensional
+        x = x.unsqueeze(1)
         new_x_shape = x.size()[:-1] + (self.num_attention_heads, self.attention_head_size)
         x = x.view(*new_x_shape)
         print("Reshaped shape:", x.shape)
         return x.permute(0, 2, 1, 3)
-        '''
-
-#Added to resolve the dimension error
-    def transpose_for_scores(self, x):
-    print("Input shape:", x.shape)
-    # Add an additional dimension to make it 4-dimensional
-    x = x.unsqueeze(1)
-    new_x_shape = x.size()[:-1] + (self.num_attention_heads, self.attention_head_size)
-    x = x.view(*new_x_shape)
-    print("Reshaped shape:", x.shape)
-    return x.permute(0, 2, 1, 3)
 
 
 
