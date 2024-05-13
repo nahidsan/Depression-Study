e3fa6acabff3334833c5ec2f25c2b4bdb1162341
Sk Nahid Sanwar <sk.nahid.sanwar@gmail.com>
2024-05-14 00:38:25 +0900

    Update model.py

--- Scripts/model.py
+++ Scripts/model.py
@@ -56,33 +56,33 @@ class SelfAttentionLayer(nn.Module):
     
 
     def forward(self, hidden_states, attention_mask):
-    mixed_query_layer = self.query(hidden_states)
-    mixed_key_layer = self.key(hidden_states)
-    mixed_value_layer = self.value(hidden_states)
+        mixed_query_layer = self.query(hidden_states)
+        mixed_key_layer = self.key(hidden_states)
+        mixed_value_layer = self.value(hidden_states)
 
-    # Reshape the mixed query, key, and value layers if needed
-    if mixed_query_layer.dim() == 3:
-        mixed_query_layer = mixed_query_layer.unsqueeze(1)
-        mixed_key_layer = mixed_key_layer.unsqueeze(1)
-        mixed_value_layer = mixed_value_layer.unsqueeze(1)
+        # Reshape the mixed query, key, and value layers if needed
+        if mixed_query_layer.dim() == 3:
+            mixed_query_layer = mixed_query_layer.unsqueeze(1)
+            mixed_key_layer = mixed_key_layer.unsqueeze(1)
+            mixed_value_layer = mixed_value_layer.unsqueeze(1)
 
-    query_layer = self.transpose_for_scores(mixed_query_layer)
-    key_layer = self.transpose_for_scores(mixed_key_layer)
-    value_layer = self.transpose_for_scores(mixed_value_layer)
+        query_layer = self.transpose_for_scores(mixed_query_layer)
+        key_layer = self.transpose_for_scores(mixed_key_layer)
+        value_layer = self.transpose_for_scores(mixed_value_layer)
 
-    attention_scores = torch.matmul(query_layer, key_layer.transpose(-1, -2))
-    attention_scores = attention_scores / math.sqrt(self.attention_head_size)
-    attention_scores = attention_scores + attention_mask
+        attention_scores = torch.matmul(query_layer, key_layer.transpose(-1, -2))
+        attention_scores = attention_scores / math.sqrt(self.attention_head_size)
+        attention_scores = attention_scores + attention_mask
 
-    attention_probs = nn.Softmax(dim=-1)(attention_scores)
-    attention_probs = self.dropout(attention_probs)
+        attention_probs = nn.Softmax(dim=-1)(attention_scores)
+        attention_probs = self.dropout(attention_probs)
+    
+        context_layer = torch.matmul(attention_probs, value_layer)
+        context_layer = context_layer.permute(0, 2, 1, 3).contiguous()
+        new_context_layer_shape = context_layer.size()[:-2] + (self.all_head_size,)
+        context_layer = context_layer.view(*new_context_layer_shape)
 
-    context_layer = torch.matmul(attention_probs, value_layer)
-    context_layer = context_layer.permute(0, 2, 1, 3).contiguous()
-    new_context_layer_shape = context_layer.size()[:-2] + (self.all_head_size,)
-    context_layer = context_layer.view(*new_context_layer_shape)
-
-    return context_layer
+        return context_layer
 
 
 
